64aa6d5df6e9e3f5747152dd5e581b2281541c1e
Yusuke Wada <yusukebe@cpan.org>
2009-11-24 21:59:04 +0900

    skelton

---
+++ lib/WWW/Yahoo/ImageDownloader.pm
@@ -0,0 +1,33 @@
+package WWW::Yahoo::ImageDownloader;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WWW::Yahoo::ImageDownloader -
+
+=head1 SYNOPSIS
+
+  use WWW::Yahoo::ImageDownloader;
+
+=head1 DESCRIPTION
+
+WWW::Yahoo::ImageDownloader is
+
+=head1 AUTHOR
+
+Yusuke Wada E<lt>yusuke at kamawada.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
